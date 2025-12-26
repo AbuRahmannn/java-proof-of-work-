0bf035e5fb723e996b2f066aab09ad03abd31ea4
Abdul Rahman <ima.rahman06@gmail.com>
2025-12-26 15:42:23 +0530

    Add files via upload

---
+++ MultiDimentionalArray.java
@@ -0,0 +1,11 @@
+public class MultiDimentionalArray {
+    public static void main(String[] args){
+        int num[][] = new int[2][4];
+        for(int i=0;i<2;i++){
+            for(int j=0;j<4;j++){
+                System.out.print(num[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
