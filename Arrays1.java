5fc13921c6e59bee4c5b43454d1aa2000a90607a
Abdul Rahman <ima.rahman06@gmail.com>
2025-12-26 14:47:44 +0530

    Add files via upload

---
+++ Arrays1.java
@@ -0,0 +1,16 @@
+
+public class Arrays1 {
+    public static void main(String[] args){
+        int  num[] = new int[4];
+        num[0] = 287;
+        num[1] = 2567;
+        num[2] = 276;
+        num[3] = 25;
+        for(int i=0;i<4;i++){
+            System.out.println(num[i]);
+        }
+
+
+    }
+    
+}
